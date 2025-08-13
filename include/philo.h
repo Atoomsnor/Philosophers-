abe0834249d738d4110bef209ea61a86277d2048
Atoomsnor <verschoor.rowan@gmail.com>
2025-08-13 12:22:46 +0200

    Beginning of phil struct

--- include/philo.h
+++ include/philo.h
@@ -6,12 +6,22 @@
 /*   By: roversch <roversch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 10:31:30 by roversch          #+#    #+#             */
-/*   Updated: 2025/08/13 11:13:34 by roversch         ###   ########.fr       */
+/*   Updated: 2025/08/13 12:06:03 by roversch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+
+typedef struct	s_philo
+{
+	int			id;
+	int			nbr_of_philos;
+	int			time_to_die;
+	int			time_to_eat;
+	int			time_to_sleep;
+	int			nbr_times_to_eat;
+}	t_philo;
 
 int	check_input(int argc, char **argv);
 
--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 /*   By: roversch <roversch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 10:15:06 by roversch          #+#    #+#             */
-/*   Updated: 2025/08/13 11:12:58 by roversch         ###   ########.fr       */
+/*   Updated: 2025/08/13 12:15:20 by roversch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,9 +35,36 @@ int	check_input(int argc, char **argv)
 	return (1);
 }
 
+void	init_philos(t_philo *phil, char **argv)
+{
+	int	i;
+	int	amount;
+
+	i = 1;
+	amount = my_atoi(argv[1]);
+	while (i < amount)
+	{
+		phil[i].id = i;
+		phil[i].nbr_of_philos = my_atoi(argv[1]);
+		phil[i].time_to_die = my_atoi(argv[2]);
+		phil[i].time_to_eat = my_atoi(argv[3]);
+		phil[i].time_to_sleep = my_atoi(argv[4]);
+		if (argv[5])
+			phil[i].nbr_times_to_eat = my_atoi(argv[5]);
+		else
+			phil[i].nbr_times_to_eat = 0;
+		printf("%d %d %d %d %d %d\n", phil[i].id, phil[i].nbr_of_philos, phil[i].time_to_die, phil[i].time_to_eat, phil[i].time_to_sleep, phil[i].nbr_times_to_eat);
+		i++;
+	}
+}
+
+
 int main(int argc, char **argv)
 {
+	t_philo	phil[200];
+
 	if (!check_input(argc, argv))
 		return (1);
+	init_philos(phil, argv);
 	printf("ye\n");
 }
