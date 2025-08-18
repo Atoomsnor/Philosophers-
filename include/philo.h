c2b9c8b63c4a2d85165b17875e5245f3daeec420
Atoomsnor <verschoor.rowan@gmail.com>
2025-08-18 13:50:36 +0200

    init_thread made and started simulation loop

--- include/philo.h
+++ include/philo.h
@@ -6,7 +6,7 @@
 /*   By: roversch <roversch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 10:31:30 by roversch          #+#    #+#             */
-/*   Updated: 2025/08/15 15:12:04 by roversch         ###   ########.fr       */
+/*   Updated: 2025/08/18 13:20:17 by roversch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,18 @@
 
 typedef struct	s_philo
 {
-	pthread_t	thread;
-	int			id;
-	int			nbr_of_philos;
-	int			time_to_die;
-	int			time_to_eat;
-	int			time_to_sleep;
-	int			nbr_times_to_eat;
-	pthread_mutex_t	*r_fork;
+	pthread_t		thread;
+	int				id;
+	int				nbr_of_philos;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				nbr_times_to_eat;
 	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	*r_fork;
 }	t_philo;
 
-//make an input struct to replace argv[1] etc?
 
-int	check_input(int argc, char **argv);
 
 int	my_atoi(const char *nptr);
 
--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 /*   By: roversch <roversch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 10:15:06 by roversch          #+#    #+#             */
-/*   Updated: 2025/08/15 15:16:57 by roversch         ###   ########.fr       */
+/*   Updated: 2025/08/18 13:49:50 by roversch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,37 +16,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int	check_input(int argc, char **argv)
+int	check_input(int argc, int amount)
 {
 	int	i;
-	int	num;
 
 	if (argc < 5 || argc > 6)
 		return (printf("Wrong amount of arguments\n"), 0);
 	i = 1;
 	while (i < argc)
 	{
-		num = my_atoi(argv[i]);
-		if (i == 1 && (num < 1 || num > 200 ))
+		if (i == 1 && (amount < 1 || amount > 200 ))
 			return (printf("Argument error\n"), 0);
-		else if (i != 1 && i != 5 && (num < 1 || num > INT_MAX))
+		else if (i != 1 && i != 5 && (amount < 1 || amount > INT_MAX))
 			return (printf("Argument error2\n"), 0);
 		i++;
 	}
 	return (1);
 }
 
-void	init_philos(t_philo *phil, char **argv)
+void	init_forks(pthread_mutex_t *fork, int amount)
 {
-	int	amount;
 	int	i;
 
 	i = 0;
-	amount = my_atoi(argv[1]);
+	while (i < amount)
+	{
+		pthread_mutex_init(&fork[i], NULL);
+		i++;
+	}
+	printf("Made %i sets of forks\n", i);
+}
+
+void	init_philos(t_philo *phil, pthread_mutex_t *fork, char **argv, int amount)
+{
+	int	i;
+
+	i = 0;
 	while (i < amount)
 	{
 		phil[i].id = i + 1;
-		phil[i].nbr_of_philos = my_atoi(argv[1]);
+		phil[i].nbr_of_philos = amount;
 		phil[i].time_to_die = my_atoi(argv[2]);
 		phil[i].time_to_eat = my_atoi(argv[3]);
 		phil[i].time_to_sleep = my_atoi(argv[4]);
@@ -54,37 +63,62 @@ void	init_philos(t_philo *phil, char **argv)
 			phil[i].nbr_times_to_eat = my_atoi(argv[5]);
 		else
 			phil[i].nbr_times_to_eat = 0;
-		printf("%d %d %d %d %d %d\n", phil[i].id, phil[i].nbr_of_philos, phil[i].time_to_die, phil[i].time_to_eat, phil[i].time_to_sleep, phil[i].nbr_times_to_eat);
+		phil[i].l_fork = &fork[i];
+		if (amount == 1) //only 1 philo means no rightfork
+			phil[i].r_fork = NULL;
+		else if (i == 0) //first philo wraps around for rightfork
+			phil[i].r_fork = &fork[amount - 1];
+		else
+			phil[i].r_fork = &fork[i - 1];
+		printf("%d %d %d %d %d %d\n", phil[i].id, phil[i].nbr_of_philos, phil[i].time_to_die, phil[i].time_to_eat,
+			phil[i].time_to_sleep, phil[i].nbr_times_to_eat);
 		i++;
 	}
 }
 
-void	init_forks(pthread_mutex_t *fork, char **argv)
+void	*phil_routine(void *pointer)
 {
-	int	amount;
+	t_philo	*phil;
+	
+	phil = (t_philo *)pointer;
+	//get time here or main?
+	printf("bigphil nr: %d is going\n", phil->id);
+	return (pointer);
+}
+
+void	init_threads(t_philo *phil, int amount)
+{
 	int	i;
 
 	i = 0;
-	amount = my_atoi(argv[1]);
 	while (i < amount)
 	{
-		pthread_mutex_init(&fork[i], NULL);
+		if (pthread_create(&phil[i].thread, NULL, phil_routine, &phil[i]) != 0)
+			printf("Die funciton goes here\n");
 		i++;
 	}
-	printf("Made %i forks\n", i);
+	i = 0;
+	while (i < amount)
+	{
+		if (pthread_join(phil[i].thread, NULL) != 0)
+			printf("Die funciton goes here\n");
+		i++;
+	}
 }
-
 
 int main(int argc, char **argv)
 {
 	t_philo			phil[MAX_PHILS];
 	pthread_mutex_t	fork[MAX_PHILS];
 
-	if (!check_input(argc, argv))
+	int	amount;
+
+	amount = my_atoi(argv[1]);
+	if (!check_input(argc, amount))
 		return (printf("uh-oh\n"), 1);
-	init_philos(phil, argv);
-	init_forks(fork, argv);
-
+	init_forks(fork, amount); //we now need to clean forks
+	init_philos(phil, fork, argv, amount); //we now need to clean phil and forks
+	init_threads(phil, amount); //something can go wrong here so clean phil and forks,
+	// printf("Die funciton goes here\n") //clean phil/forks and threads??? or time????
 	printf("ye\n");
-
 }
